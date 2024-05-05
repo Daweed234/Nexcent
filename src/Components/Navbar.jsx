1c1df2242f4aa073c6e7ef2dd7cfcb2a91c2d55e
David <ntwalidavid31@gmail.com>
2024-05-05 16:04:19 +0200

    changes

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Features</li>
+            <li>Product</li>
+            <li>Testimonials</li>
+            <li>FAQ</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
