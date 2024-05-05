5a9d1f092ed1d9dfeed3488105925cbef8da8b0a
David <ntwalidavid31@gmail.com>
2024-05-05 19:18:44 +0200

    landing page added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from '../assets/Logo.png'
 const Navbar = () => {
     return (
         <>
-            <nav className='flex justify-center items-center gap-28 bg-[#F5F7FA] px-10 py-5'>
+            <nav className='flex justify-center items-center gap-36 bg-[#F5F7FA] py-5'>
                 <div>
                     <img src={Logo} />
                 </div>
@@ -18,7 +18,7 @@ const Navbar = () => {
                 </ul>
                 <div>
                 <button className='text-green-500 px-8 py-4 rounded-md'>Login</button>
-                <button className='bg-green-500 px-8 text-white py-4 rounded-md'>Sign Up</button>
+                <button className='bg-[#4CAF4F] px-8 text-white py-4 rounded-md'>Sign Up</button>
                 </div>
             </nav>
         </>
