f67b59ccb9f7f1219fdd46fd79a48fce3d96c187
David <ntwalidavid31@gmail.com>
2024-05-05 18:56:53 +0200

    navbar made

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Logo from '../assets/Logo.png'
 
 const Navbar = () => {
     return (
         <>
-        <nav>
-            <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Features</li>
-            <li>Product</li>
-            <li>Testimonials</li>
-            <li>FAQ</li>
-            </ul>
-        </nav>
+            <nav className='flex justify-center items-center gap-28 bg-[#F5F7FA] px-10 py-5'>
+                <div>
+                    <img src={Logo} />
+                </div>
+                <ul className='flex justify-center items-center gap-10'>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Features</li>
+                    <li>Product</li>
+                    <li>Testimonials</li>
+                    <li>FAQ</li>
+                </ul>
+                <div>
+                <button className='text-green-500 px-8 py-4 rounded-md'>Login</button>
+                <button className='bg-green-500 px-8 text-white py-4 rounded-md'>Sign Up</button>
+                </div>
+            </nav>
         </>
     )
 }
